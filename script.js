d8d2f62cac9c04d4d5b2ccc22b62f5b79daab93b
minhaz1010 <minhazrahman1010@gmail.com>
2023-12-08 21:26:46 +0600

    level-02-localstorage-practice-done

--- script.js
+++ script.js
@@ -1,19 +1,81 @@
+// ? this is level 3 + level 4 practice of local storage
+
+
 window.onload = () =>{
-  displayToTheUI()
+  displayTheInputField();
 }
 
-let count = JSON.parse(localStorage.getItem('value')) || 0;
 
-document.getElementById('add-button').addEventListener('click',function(){
-  const elements = document.getElementById('count');
-  const elementValue = elements.innerText;
-  localStorage.setItem('value',JSON.stringify(++count));
-  displayToTheUI();
+let elements = document.querySelectorAll('.sent-btn');
+for(let element of elements){
+  element.addEventListener('click',function(event){
+    // let name = event.target.parentNode.querySelector('#name').value;
+    // console.log(name);
+    if(event.target.innerText === 'Send Name'){
+      let name = event.target.parentNode.querySelector('#name').value;
+      event.target.parentNode.querySelector('#name').value='';
+      saveToLocalStorage('name',name);
+    }
+    if(event.target.innerText === 'Send Email'){
+      let email = event.target.parentNode.querySelector('#email').value;
+      event.target.parentNode.querySelector('#email').value='';
+
+      saveToLocalStorage('email',email);
+    }
+    if(event.target.innerText === 'Send Message'){
+      let message = event.target.parentNode.querySelector('#message').value;
+      event.target.parentNode.querySelector('#message').value='';
+
+      saveToLocalStorage('message',message)
+    }
+  })
+}
+
+const saveToLocalStorage =(key,value) =>{
+  localStorage.setItem(key,value);
+}
+
+let deletes = document.querySelectorAll('.delete-btn');
+for(const del of deletes){
+  del.addEventListener('click',function(event){
+    if(event.target.innerText === 'Delete Name'){
+      document.getElementById('name').value='';
+
+      localStorage.removeItem('name');
+    }
+    if(event.target.innerText === 'Delete Email'){
+      document.getElementById('email').value='';
+
+      localStorage.removeItem('email');
+    }
+    if(event.target.innerText === 'Delete Message'){
+      document.getElementById('message').innerText='';
+
+      localStorage.removeItem('message');
+    }
+  })
+}
+
+function displayTheInputField(){
+  let name=(localStorage.getItem('name'));
+  let email=(localStorage.getItem('email'));
+  let message=(localStorage.getItem('message'));
+  // console.log(name,message,email);
+  document.getElementById('name').value=name;
+  document.getElementById('email').value=email;
+  document.getElementById('message').innerText=message;
+
+}
+
+
+document.getElementById('send-all-things').addEventListener('click',function(){
+ let name =   document.getElementById('name').value;
+ let email =   document.getElementById('email').value;
+ let message = document.getElementById('message').innerText;
+ let information = {};
+ information['name']=name;
+ information['email']=email;
+ information['message']=message;
+ localStorage.setItem('person',JSON.stringify(information));
+
 })
-
-let displayToTheUI = () =>{
-  let item = JSON.parse(localStorage.getItem('value'));
-  console.log(item);
-  const elements = document.getElementById('count');
-  elements.innerText=item;
-}
